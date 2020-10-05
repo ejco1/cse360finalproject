9f23bbf48b658e9dfd2b722123dcbba0ac309d3f
ejco1 <ejco1@outlook.com>
2020-10-05 09:41:28 -0700

    first commit

---
+++ assignment1/AddingMachine.java
@@ -0,0 +1,37 @@
+package cse360assignment02;
+
+public class AddingMachine {
+    private int total;
+    
+    public AddingMaching() {
+        total = 0; // not needed - included for clarity
+    }
+    public int getTotal(){
+        /**
+         * The getter for this method.
+         */
+        return 0;
+    }
+    public void add (int value) {
+        /**
+         * Adding a value to the total.
+         */
+    }
+    public void subtract (int value) {
+        /**
+         * Subtracting a value from the total.
+         */
+    }
+    public String toString() {
+        /**
+         * Returning the String version of the total.
+         */
+        return "";
+    }
+    public void clear() {
+        /**
+         * Clearing the total back to 0.
+         */
+
+    }
+}
