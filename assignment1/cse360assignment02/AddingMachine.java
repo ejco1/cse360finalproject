b40f349af48af4534efa519fdab4899e80f5e2f7
ejco1 <ejco1@outlook.com>
2020-10-05 16:12:44 -0700

    2nd commit, with a driver file.

--- assignment1/cse360assignment02/AddingMachine.java
+++ assignment1/cse360assignment02/AddingMachine.java
@@ -2,36 +2,44 @@ package cse360assignment02;
 
 public class AddingMachine {
     private int total;
+    private String record;
     
-    public AddingMaching() {
+    public AddingMachine() {
         total = 0; // not needed - included for clarity
+        record = String.valueOf(total);
     }
     public int getTotal(){
         /**
          * The getter for this method.
          */
-        return 0;
+        return total;
     }
     public void add (int value) {
         /**
          * Adding a value to the total.
          */
+        total += value;
+        record = record + " + " + String.valueOf(value);
     }
     public void subtract (int value) {
         /**
          * Subtracting a value from the total.
          */
+        total = total - value;
+        record = record + " - " + String.valueOf(value);
     }
     public String toString() {
         /**
          * Returning the String version of the total.
          */
-        return "";
+
+        return record;
     }
     public void clear() {
         /**
-         * Clearing the total back to 0.
+         * Clearing the memory.
          */
-
+        total = 0;
+        record = String.valueOf(total);
     }
 }
