12ee2eaec29bfee90e62ca3d5928c709db3e3cf6
ejco1 <ejco1@outlook.com>
2020-11-16 18:36:53 -0700

    Revert "2nd commit, with a driver file."
    This reverts commit b40f349af48af4534efa519fdab4899e80f5e2f7.

--- assignment1/AddingMachine.java
+++ assignment1/AddingMachine.java
@@ -2,44 +2,36 @@ package cse360assignment02;
 
 public class AddingMachine {
     private int total;
-    private String record;
     
-    public AddingMachine() {
+    public AddingMaching() {
         total = 0; // not needed - included for clarity
-        record = String.valueOf(total);
     }
     public int getTotal(){
         /**
          * The getter for this method.
          */
-        return total;
+        return 0;
     }
     public void add (int value) {
         /**
          * Adding a value to the total.
          */
-        total += value;
-        record = record + " + " + String.valueOf(value);
     }
     public void subtract (int value) {
         /**
          * Subtracting a value from the total.
          */
-        total = total - value;
-        record = record + " - " + String.valueOf(value);
     }
     public String toString() {
         /**
          * Returning the String version of the total.
          */
-
-        return record;
+        return "";
     }
     public void clear() {
         /**
-         * Clearing the memory.
+         * Clearing the total back to 0.
          */
-        total = 0;
-        record = String.valueOf(total);
+
     }
 }
