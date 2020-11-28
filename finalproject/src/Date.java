dc4d1bdf48a0058dcf030ac49d22ef8b9c43087d
ejco1 <ejco1@outlook.com>
2020-11-28 14:43:46 -0700

    Adding the ScatterPlot and the Date Objects. Will need to make further changes to play with the dates.

---
+++ finalproject/src/Date.java
@@ -0,0 +1,32 @@
+import java.lang.String;
+import java.util.ArrayList;
+/*
+Date Object for the last columns of the JTable. This may also be used for the JTable for the roster.
+Date objects should be held in a Date Array to be put in the last columns of the table.
+--WARNING: This does not separate students not in the roster. Will have to be done in the MyActionListener.java file.
+*/
+public class Date {
+    ArrayList<Student> arrStudents;
+    String date;
+
+/*
+Constructor for the class, setting the size of the array,
+We are given that the Date from DatePicker will be given in the form of a special String.
+*/
+public Date(String d){
+    arrStudents = new ArrayList<Student>();
+    date = d;
+}
+/*
+Array Getter.
+*/
+public ArrayList<Student> getStudent(){
+    return arrStudents;
+}
+/*
+Student Adder, to be used manually for testing purposes & through a bufferedReader for the Table purposes.
+*/
+public void addStudent(Student toBeAdded){
+    arrStudents.add(toBeAdded);
+}
+}
