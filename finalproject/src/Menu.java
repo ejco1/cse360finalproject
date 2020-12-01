093148f83918f49c6b3207de99d344d2a5fb2cc7
huntercarmona <hcarmona@asu.edu>
2020-11-30 20:10:04 -0700

    idk

--- finalproject/src/Menu.java
+++ finalproject/src/Menu.java
@@ -133,6 +133,7 @@ public class Menu extends JFrame {
 							myFile2 = fileChooser.getSelectedFile();
 							FileHandler test = new FileHandler();
 							Date testDate = test.FileReadCSVDate(myFile2, columnName);
+							System.out.println(testDate.getStudents());
 							days.add(testDate);
 							/*for(Date a : testDates)
 							{	
